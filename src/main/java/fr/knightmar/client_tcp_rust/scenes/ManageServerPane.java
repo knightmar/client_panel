f6c4216361b0dbcc08802dabcf31163d208a28cd
knightmar <74785484+knightmar@users.noreply.github.com>
2022-06-14 11:54:13 +0200

    add start and stop visual buttons (not functional)

--- src/main/java/fr/knightmar/client_tcp_rust/scenes/ManageServerPane.java
+++ src/main/java/fr/knightmar/client_tcp_rust/scenes/ManageServerPane.java
@@ -3,6 +3,7 @@ package fr.knightmar.client_tcp_rust.scenes;
 import fr.knightmar.client_tcp_rust.connexion.ConnexionManager;
 import fr.knightmar.client_tcp_rust.utils.ConnexionManagerInstanceSaver;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -16,11 +17,14 @@ import java.io.IOException;
 import java.util.Objects;
 
 import static javafx.geometry.Pos.CENTER;
+import static javafx.geometry.Pos.CENTER_LEFT;
 
 public class ManageServerPane extends BorderPane {
 
     private static boolean isConnected = true;
     private static TextArea logs;
+    private static final Button launch_server_button = new Button("Launch server");
+    private static final Button stop_server_button = new Button("Stop server");
 
     public ManageServerPane(int width, int height) throws IOException, InterruptedException {
         Text title = new Text("Manage the server");
@@ -32,18 +36,32 @@ public class ManageServerPane extends BorderPane {
         logs.setMinWidth((int) ((width / 7.0) * 4));
         logs.setMinHeight((int) ((height / 7.0) * 5));
 
-        HBox hbox = new HBox();
+
+        VBox buttons = new VBox();
+        buttons.getChildren().addAll(launch_server_button, stop_server_button);
+        buttons.setAlignment(CENTER_LEFT);
+        for (Node child : buttons.getChildren()) {
+            VBox.setMargin(child, new Insets(0, 0, 10, 0));
+            Button button = (Button) child;
+            button.setMinWidth(width / 14.0);
+        }
+        buttons.setMinWidth((int) ((width / 7.0) * 1));
+        buttons.setAlignment(CENTER);
+        this.setLeft(buttons);
+
+
+        HBox commands = new HBox();
         TextField command = new TextField();
         Button send_command = new Button("▶");
         command.setMinWidth((int) ((width / 7.0) * 4));
         send_command.setMinWidth((int) ((width / 7.0) * 0.3));
-        hbox.getChildren().addAll(command, send_command);
+        commands.getChildren().addAll(command, send_command);
 
         VBox vbox = new VBox();
         VBox.setMargin(title, new Insets(0, 0, 10, 0));
-        vbox.setMaxWidth((int) ((width / 7.0) * 4));
+        vbox.setMaxWidth((int) ((width / 7.0) * 1));
         vbox.setAlignment(CENTER);
-        vbox.getChildren().addAll(title, logs, hbox);
+        vbox.getChildren().addAll(title, logs, commands);
         this.setCenter(vbox);
 
         send_command.setOnAction(event -> {
